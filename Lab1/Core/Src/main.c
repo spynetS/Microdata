0da3ff1ea70c32a75b96b12a6b620e2b18490c70
Alfred Roos <alfred@stensatter.se>
2025-09-08 14:56:59 +0200

    update

--- Lab1/Core/Src/main.c
+++ Lab1/Core/Src/main.c
@@ -86,12 +86,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   int pressed = 0;
   while (1)
   {
